cee92455bdc155308430f340f14c9c763adb6351
HGiribola <109259008+HGiribola@users.noreply.github.com>
2023-03-06 19:37:02 -0300

    Add files via upload

---
+++ Proj1.java
@@ -0,0 +1,17 @@
+package proj1;
+
+import java.util.Scanner;
+
+public class Proj1 {
+
+    /*Copia o seu nome*/
+    public static void main(String[] args) {
+        Scanner myOBJ = new Scanner (System.in);
+        System.out.println("Digite nome, idade e RG, por obséquio");
+        String name = myOBJ.nextLine();
+        int age = myOBJ.nextInt();
+        long RG = myOBJ.nextLong(); 
+        System.out.println("Seu nome é " + name + " sua idade é " + age + " e seu RG é " + RG);
+    }
+    
+}
