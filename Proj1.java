51711ee42ac5216e731d9edcb32057da7a853cc8
HGiribola <109259008+HGiribola@users.noreply.github.com>
2023-03-07 19:45:27 -0300

    Update Proj1.java

--- Proj1.java
+++ Proj1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Proj1 {
 
-    /*Copia o seu nome*/
+    /*Copia o seu nome, idade e RG*/
     public static void main(String[] args) {
         Scanner myOBJ = new Scanner (System.in);
         System.out.println("Digite nome, idade e RG, por obséquio");
