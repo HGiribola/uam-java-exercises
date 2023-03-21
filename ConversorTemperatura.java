2f2c9a890121a870366ce6885b5676700dd03be2
Henrique da Costa Giribola <109259008+HGiribola@users.noreply.github.com>
2023-03-21 02:58:22 -0300

    Add files via upload

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -6,6 +6,11 @@ public class Main {
         System.out.println("Digite uma temperatura valida em ºC");
         double GrausCelcius = Conversor.nextDouble();
         double GrausFahrenheit = ((GrausCelcius * 9) / 5) + 32;
-        System.out.println("A temperatura em °F é " + GrausFahrenheit);
+
+        double Kelvin = GrausCelcius + 273;
+
+        System.out.printf("A temperatura em Celcius %.1fºC:" +
+                "\nEm Fahrenheit é %.1fºF e;" +
+                "\nem Kelvin é %.1fK.", GrausCelcius, GrausFahrenheit, Kelvin);
     }
 }
